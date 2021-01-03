b7217c523b857940b3ba06d393dfae48f082f770
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2021-01-03 18:22:32 +0200

    Updating

--- util/music_queue.py
+++ util/music_queue.py
@@ -2,13 +2,20 @@ class music_queue(list):
 
     def __init__(self):
         super().__init__()
-
+        
         self.__song_urls = []
         self.index = 0
 
     def __setitem__(self, title, url):
         self.append(title)
         self.__song_urls.append(url)
+
+    @property
+    def title(self):
+        if self.index not in range(0, len(self)):
+            raise IndexError
+        
+        return self[self.index]
 
     @property
     def url(self):
